d758b312eb5c28a36d1d604b78b6b956f7fec644
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-07-10 21:48:58 +0530

    hogya

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup as bs
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -7,4 +8,25 @@ def go(request):
     if request.method=="POST":
         search=request.POST['go']
         url = 'https://www.ask.com/web?q='+search
-    return render(request,'go.html')
+        res = requests.get(url)
+        soup = bs(res.text, 'lxml')
+        result_listings = soup.find_all('div', {'class': 'PartialSearchResults-item'})
+        # result_listings = soup.find_all('div', {'class': 'PartialSearchResults-item'})
+
+        final_result = []
+
+        for result in result_listings:
+            result_title = result.find(class_='PartialSearchResults-item-title').text
+            result_url = result.find('a').get('href')
+            result_desc = result.find(class_='PartialSearchResults-item-abstract').text
+
+            final_result.append((result_title, result_url, result_desc))
+
+            context = {
+            'final_result': final_result
+            }
+        return render(request, 'go.html', context)
+    
+    else:
+        return render(request, 'go.html')
+    # return render(request,'go.html')
